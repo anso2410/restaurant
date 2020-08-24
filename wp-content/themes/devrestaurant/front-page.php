f8b35e6425b99136ccda322ec7944ae208c90044
as <simana.annesophie@gmail.com>
2020-08-24 19:02:51 +0200

    homepage

--- wp-content/themes/devrestaurant/front-page.php
+++ wp-content/themes/devrestaurant/front-page.php
@@ -12,11 +12,16 @@
 <?php  get_header()?>
 </head>
 <body>
+<div class="container">
+    <div class ="bg-image  ">
+        <div class="text test">
 <?php  while(have_posts()) : the_post();?>
-<h1 class="test"> <?php the_title(); ?></h1>
+<!--<h1> <?php //the_title(); ?></h1>-->
 <?php the_content(); ?>
 <?php endwhile; ?>
-
+        </div>
+    </div>
+</div>
 <?php get_footer() ?>
 </body>
 </html>
